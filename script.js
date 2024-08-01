b29a36470bac885260b7b9c929e657110c3acaff
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-08-01 12:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downlordImages(images) {
+	return new Promise((resolve,reject) =>{
+		const img = new Image();
+		img.onload =() => resolve(img)
+		img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url} `))
+        img.src = image.url
+	})
+	
+}
+
+function displayImages(images) {
+	images.ForEach(img => output.appendChild(img));
+	
+}
+
+btn.addEventListener('click',() =>{
+	const downlordPromises = images.map((image) => downlordImages(image));
+Promise.all(downlordPromises)
+	.then(displayImages)
+	// .then((images) => displayImages(images))
+	.catch(error =>console.log(error))
+})
+
+
